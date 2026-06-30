b4659d118f89299382124899e8dbfb485fadcb9a
Amy Y <ctrlalternativedelete@gmail.com>
2026-07-01 05:53:15 +1000

    fix: extraction logic

--- src/init.ts
+++ src/init.ts
@@ -36,25 +36,20 @@ export async function init() {
         console.log(
             `found compressed files at ${compressed_files_path}, extracting (this might take a while)...`,
         );
+        Switch.setMediaPlaybackState(true);
+        console.log(`disabled sleep whilst extracting files...`);
         let compressed_files = Switch.file(compressed_files_path);
         const compressed_files_stream = compressed_files.stream();
         const reader = new zip.ZipReader(compressed_files.stream(), {
             useCompressionStream: true,
         });
         const file_name_filter_regex = /^(assets|emote-wheel|images|json|media|media-background-images|sfx|voice|webvtt)/;
-        const file_name_asset_regex = /^assets\/(.*)-.*\.(.*)$/;
         for await (const entry of reader.getEntriesGenerator()) {
             if (!entry.filename.match(file_name_filter_regex)) {
                 continue;
             }
 
-            let new_file_path: string;
-            const filename_match = entry.filename.match(file_name_asset_regex);
-            if (filename_match != null) {
-                new_file_path = `${__ROOT_PATH__}/assets/${filename_match[1]}.${filename_match[2]}`;
-            } else {
-                new_file_path = `${__ROOT_PATH__}/${entry.filename}`;
-            }
+            let new_file_path =`${__ROOT_PATH__}/${entry.filename}`;
 
             if ((await Switch.stat(new_file_path)) != null) {
                 console.log(`skipping ${entry.filename}, already exists`);
@@ -71,8 +66,30 @@ export async function init() {
             console.log(`extracted ${new_file_path}`);
         }
         await compressed_files_stream.cancel().catch(() => {});
-        console.log(`extracted all files, removing compressed files at ${compressed_files_path}...`);
-        await Switch.remove(compressed_files_path);
+        const new_compressed_files_path = `${compressed_files_path}.old`;
+        console.log(`extracted all files, renaming compressed files from ${compressed_files_path} to ${new_compressed_files_path}...`);
+        await Switch.rename(compressed_files_path, new_compressed_files_path);
+        Switch.setMediaPlaybackState(true);
+        console.log(`reenabled sleep after extracting files...`);
+    }
+
+    console.log(
+        `renaming potentially misnamed files in ${__ROOT_PATH__}/assets...`,
+    );
+    const file_name_asset_regex = /^(.*)-.*\.(.*)$/;
+    for await (const file of await Switch.readDir(`${__ROOT_PATH__}/assets`)) {
+        const filename_match = file.name.match(file_name_asset_regex);
+        if (filename_match != null) {
+            const new_file_path = `${__ROOT_PATH__}/assets/${filename_match[1]}.${filename_match[2]}`;
+            if (await Switch.stat(new_file_path) == null) {
+                await Switch.rename(`${__ROOT_PATH__}/assets/${file.name}`, new_file_path);
+                console.log(`renamed ${file.name} to ${new_file_path}`);
+            }
+            else {
+                await Switch.remove(`${__ROOT_PATH__}/assets/${file.name}`);
+                console.log(`deleted ${file.name} as ${new_file_path} already exists`);
+            }
+        }
     }
 
     window.addEventListener("beforeunload", (event) => {
